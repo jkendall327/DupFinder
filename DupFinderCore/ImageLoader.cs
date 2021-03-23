b46b4922a803f910399712c76247206d83fb2ccf
Jack Kendall <jkendall3096@gmail.com>
2021-03-23 21:08:30 +0000

    Establish project skeleton incl. unit test project.

---
+++ DupFinderCore/ImageLoader.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Drawing;
+using System.IO;
+
+namespace DupFinderCore
+{
+    public class ImageLoader : IImageLoader
+    {
+        readonly ILogger<ImageLoader> _logger;
+
+        public ImageLoader(ILogger<ImageLoader> logger)
+        {
+            _logger = logger;
+        }
+
+        public Image Load(string filepath)
+        {
+            try
+            {
+                return Image.FromFile(filepath);
+            }
+            catch (FileNotFoundException ex)
+            {
+                _logger.LogError($"File not found: {ex.FileName}. Error message: {ex.Message}. Error at {ex.TargetSite}. Stack: {ex.StackTrace}");
+
+                throw;
+            }
+        }
+    }
+}
