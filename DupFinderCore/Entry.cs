972fc5bb41571a615f58bb5fa9002d04433d1a86
Jack Kendall <jkendall3096@gmail.com>
2021-03-24 16:45:33 +0000

    Build initial skeleton for image wrapping class

---
+++ DupFinderCore/Entry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace DupFinderCore
+{
+    class Entry
+    {
+        public Image Image { get; set; }
+
+        public string Path { get; set; }
+        public int Pixels => Image.Width * Image.Height;
+        public double AspectRatio => (double)Image.Width / Image.Height;
+
+        public int FocusLevel = 64;
+
+        public long Size;
+
+        public DateTime Date;
+
+        public Image ColorMap;
+
+        public Entry(string filepath)
+        {
+            if (!File.Exists(filepath))
+            {
+                throw new FileNotFoundException();
+            }
+
+            Path = filepath;
+            Image = Image.FromFile(Path);
+
+            Size = new FileInfo(Path).Length;
+
+        }
+    }
+}
