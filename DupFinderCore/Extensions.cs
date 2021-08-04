11e39d7779e0047ccca3ef9f629604471f7a7178
Jack Kendall <jkendall3096@gmail.com>
2021-08-04 14:06:13 +0100

    Remove unused extension methods

--- DupFinderCore/Extensions.cs
+++ DupFinderCore/Extensions.cs
@@ -15,23 +15,6 @@ namespace DupFinderCore
         public static IEnumerable<(T, T)> UniquePairs<T>(this IEnumerable<T> source)
             => source.SelectMany((_, i) => source.Where((_, j) => i < j), (x, y) => (x, y));
 
-        public static Color[,] ToColorArray(this Image image) => ToColorArray((Bitmap)image);
-
-        public static Color[,] ToColorArray(this Bitmap image)
-        {
-            Color[,] array = new Color[image.Width, image.Height];
-
-            for (int i = 0; i < image.Width; i++)
-            {
-                for (int x = 0; x < image.Height; x++)
-                {
-                    array[i, x] = image.GetPixel(i, x);
-                }
-            }
-
-            return array;
-        }
-
         public static Color[] ToFlatColorArray(this Bitmap image)
         {
             List<Color> array = new();
@@ -45,35 +28,6 @@ namespace DupFinderCore
             }
 
             return array.ToArray();
-        }
-
-        public static Bitmap ToImage(this Color[,] arr)
-        {
-            // new bitmap of correct size
-            int width = arr.GetLength(0);
-            int height = arr.GetLength(1);
-            var bmp = new Bitmap(width, height);
-
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    bmp.SetPixel(x, y, arr[x, y]);
-                }
-            }
-
-            return bmp;
-        }
-
-        public static List<Color> Flatten(this Color[,] array)
-        {
-            List<Color> list = new();
-
-            foreach (var pixel in array)
-            {
-                list.Add(pixel);
-            }
-            return list;
         }
 
         //Source:
